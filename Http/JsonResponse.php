8f8288ab415b4b30e99eaf80c6379475d431fe0b
Javier Lorenzana <skqr.io@gmail.com>
2014-12-16 17:27:05 -0300

    Adding @menzow. https://github.com/GoIntegro/hateoas-bundle/pull/64

--- Http/JsonResponse.php
+++ Http/JsonResponse.php
@@ -3,6 +3,7 @@
  * @copyright 2014 Integ S.A.
  * @license http://opensource.org/licenses/MIT The MIT License (MIT)
  * @author Javier Lorenzana <javier.lorenzana@gointegro.com>
+ * @author Menzo Wijmenga <menzow@GitHub>
  */
 
 namespace GoIntegro\Hateoas\Http;
